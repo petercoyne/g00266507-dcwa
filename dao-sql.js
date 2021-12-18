fdbe09316f96ecb62d83d44f90d434ee91690093
Peter Coyne <g00266507@gmit.ie>
2021-12-18 15:03:16 +0000

    First commit

---
+++ dao-sql.js
@@ -0,0 +1,79 @@
+const mysql = require("promise-mysql")
+var pool;
+
+mysql.createPool({
+	connectionLimit: 3,
+	host: '127.0.0.1',
+	user: 'root',
+	password: '',
+	database: 'collegeDB',
+	insecureAuth : true
+})
+.then((result) => {
+	pool = result;
+})
+.catch((error) => {
+	console.log(error)
+})
+
+const getModules = () => {
+	return new Promise((resolve, reject) => {
+		pool.query("select * from module")
+		.then((result) => {
+			resolve(result)
+		})
+		.catch((error) => {
+			reject(error)
+		})
+	})
+}
+
+const getModule = (mid) => {
+	return new Promise((resolve, reject) => {
+		let query = {
+			sql: "select * from module where mid = ?",
+			values: [mid]
+		} 
+		pool.query(query)
+		.then((result) => {
+			resolve(result)
+		})
+		.catch((error) => {
+			reject(error)
+		})
+	})
+}
+
+const setModule = (mid, name, credits) => {
+	return new Promise((resolve, reject) => {
+		let query = {
+			sql: "update module set name = ?, credits = ? where mid = ?",
+			values: [name, credits, mid]
+		} 
+		pool.query(query)
+		.then((result) => {
+			resolve(result)
+		})
+		.catch((error) => {
+			reject(error)
+		})
+	})
+}
+
+const getStudentsFromModule = (mid) => {
+	return new Promise((resolve, reject) => {
+		let query = {
+			sql: "select * from student s left join student_module m on s.sid = m.sid where m.mid = ?",
+			values: [mid]
+		} 
+		pool.query(query)
+		.then((result) => {
+			resolve(result)
+		})
+		.catch((error) => {
+			reject(error)
+		})
+	})
+}
+
+module.exports = { getModules, getModule, setModule, getStudentsFromModule }
---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require("express")
+const bp = require("body-parser")
+const { check, validationResult } = require('express-validator')
+const daosql = require("./dao-sql")
+
+const app = express()
+const port = 3000
+
+app.use(bp.urlencoded({ extended: false }))
+app.set('view engine', 'ejs')
+
+app.listen(port, () => {
+	console.log("Listening on port " + port)
+})
+
+app.get('/', (req, res) => {
+	res.render("index")
+})
+
+app.get('/listModules', (req, res) => {
+	daosql.getModules()
+		.then((result) => {
+			res.render("modules", { modules: result })
+		})
+		.catch((error) => {
+			res.send(error)
+		})
+})
+
+app.get('/module/students/:mid', (req, res) => {
+	daosql.getStudentsFromModule(req.params.mid)
+		.then((result) => {
+			res.render("students", { students: result, mid: req.params.mid })
+		})
+		.catch((error) => {
+			res.send(error)
+		})
+})
+
+app.get('/modules/:mid', (req, res) => {
+	daosql.getModule(req.params.mid)
+		.then((result) => {
+			if (result.length > 0) {
+				res.render("editmodule", { mid: result[0].mid, name: result[0].name, credits: result[0].credits, errors: undefined })
+				console.log(result);
+			} else {
+				res.send(`<h3>No student with ID of ${req.params.mid}</h3>`)
+			}
+		})
+		.catch((error) => {
+			res.send(error)
+		})
+})
+
+app.post('/modules/:mid',
+	[
+		check('name').isLength({ min: 5 }).withMessage("Module name should be a minimum of 5 characters."),
+		check('credits').isIn([5, 10, 15]).withMessage("Credits must be 5, 10 or 15.")
+	],
+	(req, res) => {
+		var error = validationResult(req);
+		if (error.isEmpty()) {
+			daosql.setModule(req.params.mid, req.body.name, req.body.credits)
+				.then((result) => {
+					console.log(result);
+					res.render("editmodule", { mid: req.params.mid, name: req.body.name, credits: req.body.credits, errors: undefined, success: `OK. $` })
+				})
+				.catch((error) => {
+					res.send(error)
+				})
+		} else {
+			res.render("editmodule", { mid: req.params.mid, name: req.body.name, credits: req.body.credits, errors: error.errors })
+		}}
+)
